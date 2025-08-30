50c657089ca867231c9c4738b32efc7fa84f0e27
NehaBongarde17 <nehabongarde6@gmail.com>
2025-08-30 19:00:04 +0530

    Create LargestandSmallest.java

---
+++ LargestandSmallest.java
@@ -0,0 +1,38 @@
+package com.sunbeam;
+
+import java.util.Scanner;
+
+public class LargestandSmallest {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the size of array : ");
+		int size = sc.nextInt();
+		
+		int[] arr = new int[size];
+		System.out.print("Enter the elements in array : ");
+		
+		for(int i = 0;i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int smallest = arr[0];
+		int largest = arr[0];
+		
+		for(int i = 1 ; i < arr.length;i++) {
+			if(arr[i]<smallest) {
+				smallest = arr[i];
+			}
+			else {
+				largest = arr[i];
+			}
+			
+		}
+		
+		System.out.println("Largest : "+largest);
+		System.out.println("Smallest : "+smallest);
+		
+		sc.close();
+	}
+
+}
